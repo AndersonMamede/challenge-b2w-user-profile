754348de626459d2be2596bb52617adda9e9dbb4
AndersonMamede <mamede.anderson@gmail.com>
2019-01-16 15:33:36 -0200

    Finished mapping userDataType

--- app/src/types/index.js
+++ app/src/types/index.js
@@ -1,7 +1,31 @@
-import { string, shape } from 'prop-types';
+import { string, shape, arrayOf } from 'prop-types';
 
 const userDataType = shape({
+  image: string.isRequired,
   name: string.isRequired,
+  profession: string.isRequired,
+  description: string.isRequired,
+  contact: shape({
+    tel: string.isRequired,
+    cel: string.isRequired,
+    address: string.isRequired,
+    website: string.isRequired,
+    mail: string.isRequired,
+  }).isRequired,
+  skills: arrayOf(shape({
+    name: string.isRequired,
+    value: string.isRequired,
+  })).isRequired,
+  experience: arrayOf(shape({
+    name: string.isRequired,
+    date: string.isRequired,
+    description: string.isRequired,
+  })).isRequired,
+  education: arrayOf(shape({
+    name: string.isRequired,
+    date: string.isRequired,
+    description: string.isRequired,
+  })).isRequired,
 });
 
 export { userDataType };
