314c6dbd6b7ef549de9fdeb57bba77aefc21b640
AndersonMamede <mamede.anderson@gmail.com>
2019-01-16 17:24:36 -0200

    build

---
+++ app/build/precache-manifest.21578fe0dbaef425f02fc6e37781a5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "436541290385d26e596e3d70a8b2ef59",
+    "url": "/challenge-b2w-user-profile/app/build/static/media/favicon.43654129.ico"
+  },
+  {
+    "revision": "887ae5b407df535100d6",
+    "url": "/challenge-b2w-user-profile/app/build/static/js/runtime~main.887ae5b4.js"
+  },
+  {
+    "revision": "73209c517a445e9f9208",
+    "url": "/challenge-b2w-user-profile/app/build/static/js/main.73209c51.chunk.js"
+  },
+  {
+    "revision": "b88f8c774bd9b779b57e",
+    "url": "/challenge-b2w-user-profile/app/build/static/js/1.b88f8c77.chunk.js"
+  },
+  {
+    "revision": "73209c517a445e9f9208",
+    "url": "/challenge-b2w-user-profile/app/build/static/css/main.f7982c35.chunk.css"
+  },
+  {
+    "revision": "2ca5e32ff028139ec020119f0bebfad9",
+    "url": "/challenge-b2w-user-profile/app/build/index.html"
+  }
+];
